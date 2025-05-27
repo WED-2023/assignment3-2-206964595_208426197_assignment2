245adde8e3940dfb01d1d28c4b143aac866faa9a
IdanDuhaBGU <121314233+IdanDuhaBGU@users.noreply.github.com>
2025-05-27 13:50:34 +0300

    Update user_utils.js
    added  markaswatched

--- routes/utils/user_utils.js
+++ routes/utils/user_utils.js
@@ -1,16 +1,34 @@
 const DButils = require("./DButils");
 
 async function markAsFavorite(user_id, recipe_id){
-    await DButils.execQuery(`insert into FavoriteRecipes values ('${user_id}',${recipe_id})`);
+    await DButils.execQuery(`insert into favoriterecipes values ('${user_id}',${recipe_id})`);
 }
 
 async function getFavoriteRecipes(user_id){
-    const recipes_id = await DButils.execQuery(`select recipe_id from FavoriteRecipes where user_id='${user_id}'`);
+    const recipes_id = await DButils.execQuery(`select recipe_id from favoriterecipes where user_id='${user_id}'`);
     return recipes_id;
 }
 
+//IDAN ADDED
+async function markAsWatched(user_id, recipe_id) {
+  try {
+    // chck if watched 
+    const existing = await DButils.execQuery(
+      `SELECT * FROM watched_recipes WHERE user_id = ? AND recipe_id = ?`,
+      [user_id, recipe_id]
+    );
 
+    if (existing.length === 0) {
+      await DButils.execQuery(
+        `INSERT INTO watched_recipes (user_id, recipe_id) VALUES (?, ?)`,
+        [user_id, recipe_id]
+      );
+    }
+  } catch (err) {
+    throw err;
+  }
+}
 
-
+exports.markAsWatched = markAsWatched;
 exports.markAsFavorite = markAsFavorite;
 exports.getFavoriteRecipes = getFavoriteRecipes;
